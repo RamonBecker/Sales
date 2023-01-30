fcf58852ca41e040104081bf60c40ebfba5c7995
Ramon Becker <ramonbecker68@gmail.com>
2023-01-30 13:12:19 -0300

    Criação da API

---
+++ Sales/Sales.API/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+    
+        public OrderController() { 
+        }
+
+    }
+}
