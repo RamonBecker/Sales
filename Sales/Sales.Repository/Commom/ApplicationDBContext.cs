dc603433fda2193cdf78b0f9d556239bef8bac3e
Ramon Becker <ramonbecker68@gmail.com>
2023-03-21 18:30:59 -0300

    Adicionando Simple logging

--- Sales/Sales.Repository/Commom/ApplicationDBContext.cs
+++ Sales/Sales.Repository/Commom/ApplicationDBContext.cs
@@ -1,30 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sales.Domain;
-
+using System.Diagnostics;
 
 namespace Sales.Repository
 {
     public class ApplicationDBContext : DbContext
     {
-        // Executa: Cria o model
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Chamada da carga de mapeamento no assembly
-            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-           
-            // GetType = ApplicationDBContext 
-        }
-
         // DBSet coleção de entidades em memória, que faz acesso ao BD
-         public  DbSet<Cidade> Cidades { get; set; }
-         public  DbSet<Cliente> Clientes { get; set; }
-         public  DbSet<Produto> Produtos { get; set; }
-         public  DbSet<Pedido> Pedidos { get; set; }
-         public  DbSet<Combo> Combos { get; set; }
-         public  DbSet<PromocaoProduto> PromocoesProdutos { get; set; }
-         public  DbSet<CategoriaProduto> CategoriasProdutos { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Combo> Combos { get; set; }
+        public DbSet<PromocaoProduto> PromocoesProdutos { get; set; }
+        public DbSet<CategoriaProduto> CategoriasProdutos { get; set; }
 
         public ApplicationDBContext()
         {
@@ -37,5 +26,22 @@ namespace Sales.Repository
             //AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
         }
+
+        // Executa: Cria o model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Chamada da carga de mapeamento no assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            // GetType = ApplicationDBContext 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        }
+
     }
 }
